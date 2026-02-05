01b07affdf816aea8ba5e1397bf53ad4605824e1
Mohammed Safwan Salman <156708093+Technolap@users.noreply.github.com>
2026-02-05 06:55:42 -0330

    build the full model and trained it. Also generated predictions in the new data

--- boston_house_price_pred.R
+++ boston_house_price_pred.R
@@ -68,5 +68,70 @@ for (i in 1:k){
 all_scores
 mean(all_scores)
 
+# saving the validaition logs at each fold 
+num_epochs <- 500
+all_mae_histories <- list()
+
+for (i in 1:k) {
+  cat("Processing fold #", i, "\n")
+
+  val_indices <- which(fold_id == i)
+
+  # preparing the validation data from partition k
+  val_data <- train_data[val_indices, ]
+  val_targets <- train_targets[val_indices]
+
+  # preparing the training data: data from all other partitions 
+  partial_train_data <- train_data[-val_indices, ]
+  partial_train_targets <- train_targets[-val_indices]
+
+  # building the keras model (already compiled )
+  model <- build_model()
+
+  # train the model ( in silent mode, verbose =0)
+  history <- model %>% fit(
+    partial_train_data,
+    partial_train_targets,
+    validation_data = list(val_data, val_targets),
+    epochs = num_epochs,
+    batch_size = 16,
+    verbose = 0
+  )
+
+  mae_history <- history$metrics$val_mae
+  all_mae_histories[[i]] <- mae_history
+}
+
+all_mae_histories <- do.call(cbind, all_mae_histories)
+
+# computing average of the per epoch MAE scores for all folds
+average_mae_history <- rowMeans(all_mae_histories)
+
+# Plotting validation scores
+plot(average_mae_history, xlab = "epoch", type = 'l')
+
+# Remove early noisy epochs
+
+# plotting the validation scores, excluding the first 10 data points
+truncated_mae_history <- average_mae_history[-(1:10)]
+
+plot(average_mae_history, xlab = "epoch", type = 'l', ylim = range(truncated_mae_history)
+)
+
+# Get a fresh compiled model 
+model <- build_model()
+
+# train it on the entirety of the data
+model %>% fit(train_data, train_targets,
+              epochs = 120, batch_size = 16, verbose = 0
+)
+
+result <- model %>% evaluate(test_data, test_targets)
+
+result["mae"]
+
+# Generating predictions on new data
+predictions <- model %>% predict(test_data)
+predictions[1, ]
 
 
